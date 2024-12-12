5498a8185cdca7a6c6b02e847ee6f3b32d46fcca
shangfengh <huangs21@mails.tsinghua.edu.cn>
2024-12-12 18:51:04 +0800

    fix: :bug: fix ManiDataset

--- ManiSkillToPizzaDataset.py
+++ ManiSkillToPizzaDataset.py
@@ -1,38 +1,94 @@
 from typing import Union
+import gzip
+import json
 import h5py
 import numpy as np
 from torch.utils.data import Dataset
+from pathlib import Path
 from tqdm import tqdm
+import torch
 import tensorflow_datasets as tfds
 
-from mani_skill.utils.io_utils import load_json
-from mani_skill.utils import sapien_utils
-from mani_skill.utils import common
 from prismatic.vla.datasets import RLDSBatchTransform
 from dataclasses import dataclass
 
+def load_json(filename: Union[str, Path]):
+    filename = str(filename)
+    if filename.endswith(".gz"):
+        f = gzip.open(filename, "rt")
+    elif filename.endswith(".json"):
+        f = open(filename, "rt")
+    else:
+        raise RuntimeError(f"Unsupported extension: {filename}")
+    ret = json.loads(f.read())
+    f.close()
+    return ret
+
+# loads h5 data into memory for faster access
+def load_h5_data(data):
+    out = dict()
+    for k in data.keys():
+        if isinstance(data[k], h5py.Dataset):
+            out[k] = data[k][:]
+        else:
+            out[k] = load_h5_data(data[k])
+    return out
+
+
+def index_dict_array(x1, idx: Union[int, slice], inplace=True):
+    """Indexes every array in x1 with slice and returns result."""
+    if (
+        isinstance(x1, np.ndarray)
+        or isinstance(x1, list)
+        or isinstance(x1, torch.Tensor)
+    ):
+        return x1[idx]
+    elif isinstance(x1, dict):
+        if inplace:
+            for k in x1.keys():
+                x1[k] = index_dict_array(x1[k], idx, inplace=inplace)
+            return x1
+        else:
+            out = dict()
+            for k in x1.keys():
+                out[k] = index_dict_array(x1[k], idx, inplace=inplace)
+            return out
+
+def remove_np_uint16(x: Union[np.ndarray, dict]):
+    if isinstance(x, dict):
+        for k in x.keys():
+            x[k] = remove_np_uint16(x[k])
+        return x
+    else:
+        if x.dtype == np.uint16:
+            return x.astype(np.int32)
+        return x
+    
 class ManiSkillToPizzaDataset(Dataset):
     """
     将 ManiSkill 轨迹数据转换为 PizzaDataset 格式
     """
     
-    def __init__(self, dataset_file: str, data_dir: str, action_tokenizer, processor, image_transform, prompt_builder_fn, load_count=-1, success_only=False, device=None) -> None:
-        self.data_dir = data_dir
-        self.batchTransform = RLDSBatchTransform(
+    def __init__(self, dataset_file: str, action_tokenizer, processor, image_transform, prompt_builder_fn, load_count=-1, success_only=False, device=None) -> None:
+        '''self.batchTransform = RLDSBatchTransform(
             action_tokenizer,
             processor.tokenizer,
             image_transform,
             prompt_builder_fn)
-        
+        '''
         # 加载 ManiSkill 数据集
         self.dataset_file = dataset_file
-        self.device = device
-        self.data = h5py.File(dataset_file, "r")
+        # self.device = device
+        self.h5data = h5py.File(dataset_file, "r")
         json_path = dataset_file.replace(".h5", ".json")
         self.json_data = load_json(json_path)
         self.episodes = self.json_data["episodes"]
         self.env_info = self.json_data["env_info"]
         
+        #并不有用
+        self.env_id = self.env_info["env_id"]
+        self.env_kwargs = self.env_info["env_kwargs"]
+
         self.data = []
         
         # 加载和处理轨迹数据
@@ -40,28 +96,58 @@ class ManiSkillToPizzaDataset(Dataset):
             load_count = len(self.episodes)
         for eps_id in tqdm(range(load_count)):
             eps = self.episodes[eps_id]
+
             if success_only:
+                assert "success" in eps, "episodes in this dataset do not have the success attribute, cannot load dataset with success_only=True"
                 if not eps.get("success", False):
                     continue
-            trajectory = self.data[f"traj_{eps['episode_id']}"]
+            trajectory = self.h5data[f"traj_{eps['episode_id']}"]
             trajectory = load_h5_data(trajectory)
             eps_len = len(trajectory["actions"])
             
-            # 排除最后一个观测
-            obs = common.index_dict_array(trajectory["obs"], slice(eps_len))
-            
+            obs = index_dict_array(trajectory["obs"], slice(eps_len))
+            obs = remove_np_uint16(obs)
+
             # 将数据转换为 PizzaDataset 格式
             for idx in range(eps_len - 1):
                 data_pack = {}
                 data_pack["dataset_name"] = "ManiSkill"
                 data_pack["action"] = [trajectory["actions"][idx]]
                 data_pack["observation"] = {}
-                data_pack["observation"]["image_primary"] = [obs["image"][idx]]  # 假设图像在观测中
+                data_pack["observation"]["image_primary"] = {}  # 假设图像在观测中
                 data_pack["task"] = {}
                 data_pack["task"]["language_instruction"] = eps.get("language_instruction", "No instruction")  # 假设有语言指令
-                
+            
                 self.data.append(data_pack)
-    
+                
+                # handle data that might optionally be in the trajectory
+                '''
+                if "rewards" in trajectory:
+                    if self.rewards is None:
+                        self.rewards = [trajectory["rewards"]]
+                    else:
+                        self.rewards.append(trajectory["rewards"])
+                if "success" in trajectory:
+                    if self.success is None:
+                        self.success = [trajectory["success"]]
+                    else:
+                        self.success.append(trajectory["success"])
+                if "fail" in trajectory:
+                    if self.fail is None:
+                        self.fail = [trajectory["fail"]]
+                    else:
+                        self.fail.append(trajectory["fail"])
+                
+                if self.rewards is not None:
+                    self.rewards = np.concatenate(self.rewards)
+                if self.success is not None:
+                    self.success = np.concatenate(self.success)
+                if self.fail is not None:
+                    self.fail = np.concatenate(self.fail)
+
+                self.terminated.append(trajectory["terminated"])
+                self.truncated.append(trajectory["truncated"])
+                '''
     def __len__(self):
         return len(self.data)
     
@@ -74,21 +160,16 @@ if __name__ == "__main__":
     from prismatic.models.backbones.llm.prompting import PurePromptBuilder
     import time
 
-    # 初始化模型和处理器
-    vla_path = '/path/to/vla_param'
-    processor = AutoProcessor.from_pretrained(vla_path, trust_remote_code=True)
-    action_tokenizer = ActionTokenizer(processor.tokenizer)
-    
-    # 初始化数据集
-    data_dir = '/path/to/pizza_dataset/'
-    dataset_file = '/path/to/mani_skill_data.h5'
+    vla_path = None #'/home/yunqiliu/vlatune/transfer/openvla_param'
+    processor = None #AutoProcessor.from_pretrained(vla_path, trust_remote_code=True)
+    action_tokenizer = None #ActionTokenizer(processor.tokenizer)
+    dataset_file = '/home/v-wenhuitan/hs/simplerEnv/data_maniskill/PlugCharger-v1/motionplanning/trajectory.h5'
     
     dataset = ManiSkillToPizzaDataset(
         dataset_file=dataset_file,
-        data_dir=data_dir,
         action_tokenizer=action_tokenizer,
         processor=processor,
-        image_transform=processor.image_processor.apply_transform,
+        image_transform=None, #processor.image_processor.apply_transform,
         prompt_builder_fn=PurePromptBuilder
     )
     
